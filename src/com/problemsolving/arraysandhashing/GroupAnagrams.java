5c103cdbe1c5ea5917e9da8b14acccfbc07591f9
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2024-01-12 16:11:00 +0200

    Solve GroupAnagrams

--- src/com/problemsolving/arraysandhashing/GroupAnagrams.java
+++ src/com/problemsolving/arraysandhashing/GroupAnagrams.java
@@ -9,7 +9,6 @@ import java.util.Map;
 public class GroupAnagrams {
   public List<List<String>> groupAnagrams(String[] strs) {
     Map<String, List<String>> map = new HashMap<>();
-
     for (String word : strs) {
       char[] chars = word.toCharArray();
       Arrays.sort(chars);
