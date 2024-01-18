a1645f6b70703c8d8b93f332438b851af8169cfe
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2024-01-18 16:43:07 +0200

    Solve TopKFrequentElement

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 package src;
 
 import src.com.problemsolving.arraysandhashing.GroupAnagrams;
+import src.com.problemsolving.arraysandhashing.TopKFrequentElement;
 
 public class Main {
   public static void main(String[] args) {
-    GroupAnagrams groupAnagrams = new GroupAnagrams();
-    var x = groupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    TopKFrequentElement topKFrequentElement = new TopKFrequentElement();
+    var x = topKFrequentElement.topKFrequentBucketSort(new int[]{1,1,1,2,2,3},2);
     for (var sub : x
     ) {
-      System.out.println(sub.toString());
+      System.out.println(sub);
     }
   }
 }
---
+++ src/com/problemsolving/arraysandhashing/TopKFrequentElement.java
@@ -0,0 +1,64 @@
+package src.com.problemsolving.arraysandhashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class TopKFrequentElement {
+  public int[] topKFrequent(int[] nums, int k) {
+    List<Integer> sortedKeys = new ArrayList<>();
+    int[] result = new int[k];
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      if (map.containsKey(num)) {
+        map.put(num, map.get(num) + 1);
+      } else {
+        map.put(num, 1);
+      }
+    }
+    map.entrySet()
+        .stream()
+        .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+        .forEach((integer, integer2) -> sortedKeys.add(integer));
+    for (int i = 0; i<k;i++)
+    {
+      result[i] = sortedKeys.get(i);
+    }
+    return result;
+  }
+
+  public int[] topKFrequentBucketSort(int[] nums, int k) {
+    List<Integer>[] bucket = new ArrayList[nums.length + 1];;
+    int[] result = new int[k];
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      if (map.containsKey(num)) {
+        map.put(num, map.get(num) + 1);
+      } else {
+        map.put(num, 1);
+      }
+    }
+    map.keySet().forEach(key -> {
+      int freq = map.get(key);
+      if (Objects.isNull(bucket[freq])) {
+        bucket[freq] = new ArrayList<>();
+      }
+      bucket[freq].add(key);
+    });
+    int index = 0;
+
+    for (int i = nums.length; i >= 0; i--)
+      if (bucket[i] != null)
+        for (int val : bucket[i]){
+          result[index++] = val;
+          if(index == k)
+            return result;
+        }
+     return result;
+  }
+}
